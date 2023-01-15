0282469138194a6e8a8f05be392e9e0f37290730
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-01-15 20:00:18 +0530

    feat: add function to check if string ends with "script"

---
+++ isEndWithScript.js
@@ -0,0 +1,6 @@
+const isEndWithScriptArrowFunction = (string) => string.endsWith("Script")
+
+function isEndWithScriptRegularFunction(string) { return string.endsWith("Script") }
+
+module.exports = { isEndWithScriptArrowFunction, isEndWithScriptRegularFunction }
+
---
+++ isEndWithScript.test.js
@@ -0,0 +1,15 @@
+const { isEndWithScriptArrowFunction, isEndWithScriptRegularFunction } = require('./isEndWithScript')
+
+test('test whether a string end with "Script"(case-sensitive) using Arrow function', () => {
+    expect(isEndWithScriptArrowFunction("javaScript")).toBe(true)
+    expect(isEndWithScriptArrowFunction("javascript")).toBe(false)
+    expect(isEndWithScriptArrowFunction("javaScriptisGood")).toBe(false)
+    expect(isEndWithScriptArrowFunction("cript")).toBe(false)
+})
+
+test('test whether a string end with "Script"(case-sensitive) using Regular function', () => {
+    expect(isEndWithScriptRegularFunction("javaScript")).toBe(true)
+    expect(isEndWithScriptRegularFunction("javascript")).toBe(false)
+    expect(isEndWithScriptRegularFunction("javaScriptisGood")).toBe(false)
+    expect(isEndWithScriptRegularFunction("cript")).toBe(false)
+})
