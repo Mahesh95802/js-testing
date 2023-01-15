614fbd1c3cb62f1079cac905a441329edb58948b
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-01-15 20:01:55 +0530

    feat: add function to check if given number consists of the same digit

---
+++ isSameDigitedNumber.js
@@ -0,0 +1,19 @@
+// import * as _ from 'lodash';
+const _ = require("lodash")
+
+const isSameDigitedNumberArrowFunction = (number) => {
+    if(!_.isNumber(number)) return false
+    number = String(number)
+    for(let i = 0; i < number.length - 1; i++) if(number[i] != number[i+1]) return false
+    return true
+}
+
+function isSameDigitedNumberRegularFunction(number) {
+    if(!_.isNumber(number)) return false
+    number = String(number)
+    for(let i = 0; i < number.length - 1; i++) if(number[i] != number[i+1]) return false
+    return true
+}
+
+
+module.exports = { isSameDigitedNumberArrowFunction, isSameDigitedNumberRegularFunction }
---
+++ isSameDigitedNumber.test.js
@@ -0,0 +1,15 @@
+const { isSameDigitedNumberArrowFunction, isSameDigitedNumberRegularFunction } = require("./isSameDigitedNumber")
+
+test("check whether all the digits in a given number are the same or not using Arrow function", () => {
+    expect(isSameDigitedNumberArrowFunction(123456789)).toBe(false)
+    expect(isSameDigitedNumberArrowFunction(22222)).toBe(true)
+    expect(isSameDigitedNumberArrowFunction("222222")).toBe(false)
+    expect(isSameDigitedNumberArrowFunction("aaaaaa")).toBe(false)
+})
+
+test("check whether all the digits in a given number are the same or not using Regular function", () => {
+    expect(isSameDigitedNumberRegularFunction(123456789)).toBe(false)
+    expect(isSameDigitedNumberRegularFunction(22222)).toBe(true)
+    expect(isSameDigitedNumberRegularFunction("222222")).toBe(false)
+    expect(isSameDigitedNumberRegularFunction("aaaaaa")).toBe(false)
+})
