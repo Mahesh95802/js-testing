c9021e4e08e381aedcd29027c8a82d78fb07495c
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-01-15 19:59:46 +0530

    feat: add function to Concatenate 2 strings except their 1st character

---
+++ concatinateStringsWithoutFirstChar.js
@@ -0,0 +1,15 @@
+const concatinateStringsWithoutFirstCharArrowFunction = (array) => {
+    if(array.length !== 2 ) return "Invalid Input"
+    let concatinatedString = '';
+    array.forEach( string => concatinatedString += string.slice(1) );
+    return concatinatedString
+}
+
+function concatinateStringsWithoutFirstCharRegularFunction(array) {
+    if(array.length !== 2 ) return "Invalid Input"
+    let concatinatedString = '';
+    array.forEach( string => concatinatedString += string.slice(1) );
+    return concatinatedString
+}
+
+module.exports = { concatinateStringsWithoutFirstCharArrowFunction, concatinateStringsWithoutFirstCharRegularFunction }
